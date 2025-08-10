fc12fe8c49a16267f92e544f90e2a2360c1d5e18
Avverest <aominedaiki3ww@gmail.com>
2025-08-10 22:10:33 +0300

    Init nvim konfig from kikstart.

---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,41 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        -- Стандартные маппинги
+        ['<cr>'] = 'open', -- Enter для открытия файла/папки
+        ['l'] = 'open', -- 'l' для открытия
+        ['h'] = 'close_node', -- 'h' для закрытия папки
+        ['z'] = 'close_all_nodes', -- 'z' закрыть все подпапки
+
+        -- Пользовательские маппинги
+        ['<C-t>'] = 'open_tabnew', -- Ctrl+t: открыть в новой вкладке
+        ['<C-f>'] = 'focus_preview', -- Ctrl+f: фокус на превью
+        ['<C-e>'] = 'close_window', -- Ctrl+e: закрыть neo-tree
+        ['R'] = 'refresh', -- R: обновить
+        ['a'] = 'add', -- a: создать файл/папку
+        ['d'] = 'delete', -- d: удалить
+        ['r'] = 'rename', -- r: переименовать
+        ['yy'] = 'copy_to_clipboard', -- yy: копировать
+        ['pp'] = 'paste_from_clipboard', -- pp: вставить
+        },
+      },
+    },
+  },
+}
