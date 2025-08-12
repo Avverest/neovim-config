1de98baf1a4adeec86ba01d0276f5b28974e4c9e
averest <aominedaiki3ww@gmail.com>
2025-08-12 14:22:43 +0300

    Add buffers commands to neotree.

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '|', ':Neotree buffers<CR>', desc = 'NeoTree buffers reveal', silent = true },
   },
   opts = {
     filesystem = {
@@ -47,6 +48,24 @@ return {
             },
           },
         },
+      },
+      buffers = {
+        ['\\'] = 'close_window',
+        ['d'] = 'buffer_delete',
+        ['bd'] = 'buffer_delete',
+        ['<bs>'] = 'navigate_up',
+        ['.'] = 'set_root',
+        ['o'] = {
+          'show_help',
+          nowait = false,
+          config = { title = 'Order by', prefix_key = 'o' },
+        },
+        ['oc'] = { 'order_by_created', nowait = false },
+        ['od'] = { 'order_by_diagnostics', nowait = false },
+        ['om'] = { 'order_by_modified', nowait = false },
+        ['on'] = { 'order_by_name', nowait = false },
+        ['os'] = { 'order_by_size', nowait = false },
+        ['ot'] = { 'order_by_type', nowait = false },
       },
     },
   },
