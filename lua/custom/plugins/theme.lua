b9fd92c81b07230efcfc9a0d1dd1245b29fa051f
averest <aominedaiki3ww@gmail.com>
2025-08-19 16:52:05 +0300

    Add themes.

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -7,16 +7,47 @@
 --   end,
 -- }
 
+-- return {
+--   'vague2k/vague.nvim',
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other plugins
+--   config = function()
+--     -- NOTE: you do not need to call setup if you don't want to.
+--     require('vague').setup {
+--       -- optional configuration here
+--       transparent = true,
+--     }
+--     vim.cmd 'colorscheme vague'
+--   end,
+-- }
+
+-- return {
+--   'ellisonleao/gruvbox.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require 'gruvbox'
+--     vim.cmd 'colorscheme gruvbox'
+--   end,
+-- }
+
+-- return {
+-- 	'yorik1984/newpaper.nvim',
+--   priority = 1000,
+-- 	config = function()
+--     require 'newpaper'
+--     vim.cmd 'colorscheme gruvbox'
+--   end,
+-- }
+
 return {
-  'vague2k/vague.nvim',
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other plugins
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    -- NOTE: you do not need to call setup if you don't want to.
-    require('vague').setup {
-      -- optional configuration here
-      transparent = true,
-    }
-    vim.cmd 'colorscheme vague'
+    require('github-theme').setup {}
+
+    vim.cmd 'colorscheme github_dark_dimmed'
   end,
 }
